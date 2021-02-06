93d04b1a14da8ab202aa6b17655c99f99785f481
Laura Bilíkova <bilikovalaura@bilikova.onmicrosoft.com>
2021-02-06 18:46:14 +0100

    Yesss

---
+++ Inventory/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inventory
+{
+    class Inventory
+    {
+        class Knight
+        {
+            class Weapons
+            {
+                List<Weapons> weapons = new List<Weapons>();
+                weapons.add(Sword);
+
+            }
+            class Consumables
+            {
+
+            }
+            class Armor
+            {
+
+            }
+            class Abilities 
+            {
+
+            }
+        }
+        
+    }
+}
